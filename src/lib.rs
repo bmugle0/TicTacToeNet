69f43f44bb30e69612575e1d129c611140178553
Brendon M <bmugle0@gmail.com>
2025-07-30 13:29:16 -0700

    Added Recombination and tidied up main.rs by moving the main game loop into a function in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,32 @@ impl Network {
         Self { layers }
     }
     
-    pub fn recombine(&self, other: Self) -> Self {
-        //This will take two networks and randomly take parts from each one to make a new one
-        todo!()
+    //This will take two networks and randomly take parts from each one to make a new one
+    pub fn recombine(&self, other: &Self) -> Self {
+        let mut rng = rand::thread_rng();
+        let mut result: Network = self.clone();
+        
+        for (layer_a, layer_b) in result.layers.iter_mut().zip(&other.layers) {
+            for (neuron_a, neuron_b) in layer_a.neurons.iter_mut().zip(&layer_b.neurons) {
+                //Random Chance that result's neuron's bias will be replaced by other's corresponding bias
+                if rng.r#gen() {
+                    neuron_a.bias = neuron_b.bias;
+                }
+                
+                for (w_a, w_b) in neuron_a.weights.iter_mut().zip(&neuron_b.weights) {
+                    //Same thing as above, but instead with each weight
+                    if rng.r#gen() {
+                        *w_a = *w_b;
+                    }
+                }
+            }
+        }
+        
+        result
     }
-    
+
+    //This will take one network and make a single random change somewhere in the network
     pub fn mutate(&self) -> Self {
-        //This will take one network and make a single random change somewhere in the network
         todo!()
     }
 }
@@ -148,5 +167,68 @@ impl TTTBoard {
         );
         
         check.iter().any(|item| *item)
+    }
+    
+    pub fn run_game<'a>(player1: &'a Network, player2: &'a Network) -> (&'a Network, GameResult) {
+        //println!("Starting Check");
+        let mut board = Self::new();
+        let mut turns: usize = 0;
+        let result = loop {
+            if (turns & 1) == 0 {
+                //println!("Player 1's turn");
+                if let Ok(_) = player1.play_move(&mut board) {}
+                else {
+                    break (player2, GameResult::Error(turns));
+                }
+            }
+            else {
+                //println!("Player 2's turn");
+                if let Ok(_) = player2.play_move(&mut board) {}
+                else {
+                    break (player1, GameResult::Error(turns));
+                }
+            }
+            if board.is_done() {
+                //println!("Game Won");
+                break (match (turns & 1) == 0 {
+                    true => player1,
+                    false => player2,
+                }, GameResult::Win )
+            }
+            
+            turns += 1;
+            board.switch_view();
+        };
+        return result
+    }
+}
+
+#[derive(Debug)]
+pub enum GameResult {
+    Win,
+    Error(usize),
+}
+    
+trait TTTPlayer {
+        fn play_move(&self, board: &mut TTTBoard) -> Result<(), ()>;
+}
+    
+impl TTTPlayer for Network {
+    fn play_move(&self, board: &mut TTTBoard) -> Result<(), ()> {
+        let output = self.propagate(
+            board.places
+            .iter()
+            .map(|value| (*value as f32))
+            .collect::<Vec<_>>()
+        ).iter()
+        .map(|value| value.round() as usize)
+        .collect::<Vec<_>>();
+        
+        if (output[0] + (3 * output[1])) < 9 
+        && board.places[output[0] + (3 * output[1])] != 0 {
+            return Err(())
+        }
+        
+        board.place_sign(1, (output[0], output[1]))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use big_brain::{Network, LayerTopology, TTTBoard};
+use big_brain::{Network, LayerTopology, TTTBoard, GameResult,};
 
 fn main() {
     //Tic Tac Toe Network structure
@@ -25,65 +25,22 @@ fn main() {
         .collect();
     
     //Loop should start here
-    let mut next_brain_pool: Vec<Network> = Vec::with_capacity(50);
+    let mut pool_queue: Vec<(&Network, GameResult)> = Vec::with_capacity(50);
     for pair in paired_brains {
-        println!("Starting Check");
-        let mut board = TTTBoard::new();
-        let mut turn: bool = true;
-        let result = loop {
-            if turn {
-                println!("Player 1's turn");
-                if let Ok(_) = pair.0.play_move(&mut board) {}
-                else {
-                    break pair.1;
-                }
-            }
-            else {
-                println!("Player 2's turn");
-                if let Ok(_) = pair.1.play_move(&mut board) {}
-                else {
-                    break pair.0;
-                }
-            }
-            if board.is_done() {
-                println!("Game Won");
-                break match turn {
-                    true => pair.0,
-                    false => pair.1,
-                }
-            }
-            
-            turn = !turn;
-            board.switch_view();
-        };
-        next_brain_pool.push(result.clone());
+        let result = TTTBoard::run_game(pair.0, pair.1);
+        pool_queue.push(result);
     }
     
-    brain_pool = next_brain_pool;
+    //Order Game Results by how well the networks did. Win first, followed by Error(n) with n in descending order.
+    pool_queue.sort_by_key(|( _, k)| match k {
+        GameResult::Win => (0, 0),
+        GameResult::Error(n) => (1, usize::MAX - n),
+    });
+    println!("{:?}", pool_queue.iter().map(|x| &x.1 ).collect::<Vec<_>>());
     
-    trait TTTPlayer {
-        fn play_move(&self, board: &mut TTTBoard) -> Result<(), ()>;
-    }
-    
-    impl TTTPlayer for Network {
-        fn play_move(&self, board: &mut TTTBoard) -> Result<(), ()> {
-            let output = self.propagate(
-                board.places
-                .iter()
-                .map(|value| (*value as f32))
-                .collect::<Vec<_>>()
-            ).iter()
-            .map(|value| value.round() as usize)
-            .collect::<Vec<_>>();
-            
-            if (output[0] + (3 * output[1])) < 9 
-            && board.places[output[0] + (3 * output[1])] != 0 {
-                return Err(())
-            }
-            
-            board.place_sign(1, (output[0], output[1]))
-        }
-    }
+    println!("Brain 1: {:?}", pool_queue[0].0);
+    println!("Brain 2: {:?}", pool_queue[1].0);
+    println!("Brain Baby: {:?}", pool_queue[0].0.recombine(&pool_queue[1].0));
 
     //This is my debug area
     //Do not touch! JK
